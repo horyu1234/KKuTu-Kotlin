47015217f7435c3c4369e8aecaf907e5e7b59752
horyu1234 <horyu1234@naver.com>
2017-03-23 22:26:14 +0900

    Initial commit

---
+++ src/main/java/com/horyu1234/kkutu_game/KKutuGameApplication.java
@@ -0,0 +1,13 @@
+package com.horyu1234.kkutu_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class KKutuGameApplication {
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(KKutuGameApplication.class, args);
+    }
+}
---
+++ src/test/java/com/horyu1234/kkutu_game/KKutuGameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.horyu1234.kkutu_game;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KKutuGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
