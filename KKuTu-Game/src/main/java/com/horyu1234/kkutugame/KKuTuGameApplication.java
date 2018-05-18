6856ce91d71e216a62fcfa2aa0daca272acbfc1b
datablue <sbcho@datablue.co.kr>
2018-05-18 21:20:10 +0900

    test commit message
    Change-Id: I6aa0a1976ab81220c1dd6ae409749b506d2f3623

--- KKuTu-Game/src/main/java/com/horyu1234/kkutugame/KKuTuGameApplication.java
+++ KKuTu-Game/src/main/java/com/horyu1234/kkutugame/KKuTuGameApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KKuTuGameApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KKuTuGameApplication.class, args);
 	}
